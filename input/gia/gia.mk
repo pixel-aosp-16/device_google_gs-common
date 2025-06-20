f6464d37c255bb6a0fd7cb7fb7d2f1f06e0025cf
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-20 10:59:19 +0000

    remove references to gia manifest file from device repo

--- input/gia/gia.mk
+++ input/gia/gia.mk
@@ -3,5 +3,5 @@ BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/input/gia/sepolicy
 # TODO: This probably should be removed, but need to verify it doesn't exist. Difficult to grep for.
 PRODUCT_PACKAGES += gia
 
-DEVICE_MANIFEST_FILE += device/google/gs-common/input/gia/aidl/manifest.xml
+# DEVICE_MANIFEST_FILE += device/google/gs-common/input/gia/aidl/manifest.xml
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/input/gia/aidl/compatibility_matrix.xml
